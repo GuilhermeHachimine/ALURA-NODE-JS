d8929c2f522067a911a94e67cf2d4ee5ad44f25e
Guilherme Hachimine <gui.hachimine@gmail.com>
2021-07-21 09:23:05 -0300

    FIX: migration to windows

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const customExpress = require('./config/customExpress');
-
+const consign = require('consign');
 const app = customExpress();
 
+    consign()
+    .include('controllers')
+    .into(app);
 
 app.listen(3000,()=>console.log('servidor rodando na porta 3000'));
