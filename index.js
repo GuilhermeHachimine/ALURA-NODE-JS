3fca23036d65347fbd90cf3525a5f8b42318b84d
Guilherme Hachimine <gui.hachimine@gmail.com>
2021-07-21 09:57:36 -0300

    npm installs

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const customExpress = require('./config/customExpress');
-const consign = require('consign');
 const app = customExpress();
 
-    consign()
-    .include('controllers')
-    .into(app);
+
 
 app.listen(3000,()=>console.log('servidor rodando na porta 3000'));
